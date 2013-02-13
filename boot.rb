67273da72a2fba603fdbc50049420f767d564ce4
Vagner Zampieri <vgzampieri@gmail.com>
2013-02-13 14:41:25 +0000

    Boot load

---
+++ boot.rb
@@ -0,0 +1,16 @@
+# Load paths
+APP_ROOT = File.dirname(__FILE__)
+
+load_paths = %w[. lib]
+
+load_paths.each do |path|
+  $:.unshift(File.join(APP_ROOT, path))
+end
+
+require 'rubygems'
+require 'bundler/setup'
+require 'fileutils'
+require 'yaml'
+require 'map'
+
+require 'mount_directory'
