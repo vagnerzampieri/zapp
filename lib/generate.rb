50af9006e16551e7bf837b8a9fdfc5ab1451355b
Vagner Zampieri <vgzampieri@gmail.com>
2013-02-28 00:34:48 +0000

    Check unless test_unit

--- lib/generate.rb
+++ lib/generate.rb
@@ -1,13 +1,13 @@
 module Generate
   def rails
     Dir.chdir(path) do
-      system "rails new #{name} #{database.adapter}#{without_test_unit?} --skip-gemfile --skip-bundle"
+      system "rails new #{name} #{database.adapter}#{test_unit?} --skip-gemfile --skip-bundle"
     end
   end
 
   private
-  def without_test_unit?
-    " -T" if test == 'rspec'
+  def test_unit?
+    " -T" unless test == 'test_unit'
   end
 
   def database
