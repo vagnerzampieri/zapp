9c3fc5b67c9547aae4b89a37ea15dfb5c47d0f01
Vagner Zampieri <vgzampieri@gmail.com>
2013-03-01 02:19:56 +0000

    Skip to git

--- lib/generate.rb
+++ lib/generate.rb
@@ -1,16 +1,32 @@
 module Generate
-  def rails
+  def rails_new
     Dir.chdir(path) do
-      system "rails new #{name} #{database.adapter}#{test_unit?} --skip-gemfile --skip-bundle"
+      system create_application
     end
   end
 
   private
-  def test_unit?
-    " -T" unless test == 'test_unit'
+  def create_application
+    new = "rails new #{name} #{database.adapter}"
+    new << skip_gemfile!
+    new << skip_test_unit?
+    new << skip_git?
+    new
   end
 
   def database
     "-d #{database.adapter}"
+  end
+
+  def skip_gemfile!
+    " --skip-gemfile --skip-bundle"
+  end
+  
+  def skip_test_unit?
+    " -T" unless test == 'test_unit'
+  end
+
+  def skip_git?
+    " -G" if git.has_key?(:gitignore)
   end
 end
